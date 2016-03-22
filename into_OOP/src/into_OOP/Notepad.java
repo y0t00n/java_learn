a81e05e2a6cc9d9b886f9c1e4988c4b83f8f2e95
Ievgen Lauta <ievgen.lauta@gmail.com>
2016-03-22 14:58:02 +0200

    Notepad Speech text generator

--- into_OOP/src/into_OOP/Notepad.java
+++ into_OOP/src/into_OOP/Notepad.java
@@ -22,7 +22,8 @@ public class Notepad {
 		}
 		
 		public String toString(){
-			return (this.getDate() + " : " + this.getText());
+			//return (this.getDate() + " : " + this.getText());
+			return (this.getText());
 		}
 		
 		public void setText(String t){
@@ -47,6 +48,15 @@ public class Notepad {
 		
 	}
 	
+	public Notepad(){}
+	public Notepad(Record r){}
+	
+	public static Notepad createNotepad(){
+		Notepad newNotepad = new Notepad();
+		newNotepad.notes = new ArrayList<Record>();
+		return newNotepad;
+	}
+	
 	ArrayList<Record> notes = new ArrayList<Record>();
 	
 //	String Owner;
@@ -68,5 +78,91 @@ public class Notepad {
 			System.out.println(i++ + " " + r);
 		}
 	}
+	
+	public static Notepad generateNotepad(int n){
+		
+		//frazes:
+		
+		ArrayList<String> start = new ArrayList<>(Arrays.asList(
+			"Товарищи!", 
+			"С другой стороны", 
+			"Равным образом", 
+			"Не следует, однако, забывать, что", 
+			"Таким образом", 
+			"Повседневная практика показывает, что",
+			"Удивительно также то, что",
+			"Думаю со мной согласятся все, что",
+			"Очевидно, что",
+			"Не смотря ни на что, но и вопреки всему",
+			"Даже вахтёру понятно, что",
+			"Из этого можно сделать интересный вывод -",
+			"Неимоверно, но факт -"
+		));
+
+		ArrayList<String> actor = new ArrayList<>(Arrays.asList(
+			"реализация намеченных плановых заданий", 
+			"рамки и место обучения кадров", 
+			"постоянный количественный рост и сфера нашей активности", 
+			"сложившаяся структура организации", 
+			"новая модель организационной деятельности", 
+			"дальнейшее развитие различных форм деятельности",
+			"суперпозиция разновекторных тенденций",
+			"эффект абстрагированного воприятия объективных фактов",
+			"заведомо проигрышная упадническая политика прнятия стратегических решений",
+			"непреодолимая тяга многих руководителей к минимизации расходов",
+			"классический подход к интерпретации нелинейных процессов макроэкономического и геополитеческого масштаба"
+		));
+
+		ArrayList<String> action = new ArrayList<>(Arrays.asList(
+			"играет важную роль в формировании", 
+			"требует от нас анализа", 
+			"требует определения и уточнений", 
+			"способствует подготовке и реализации", 
+			"обеспечивает широкому кругу (специалистов) участие в формировании", 
+			"позволяет выполнить важные задания по разработке",
+			"заставляет иначе вглянуть на шаблоны в осмыслении",
+			"обрекает систему на применение существенных преобразований",
+			"подразумевает ре-акционные процессы вместо освоения",
+			"непосредственно гарантирует некоторым руководителям преимущество в обуздании"
+		));
+
+		ArrayList<String> result = new ArrayList<>(Arrays.asList(
+			"существенных финансовых и административных условий", 
+			"дальнейших направлений развития", 
+			"системы массового участия", 
+			"позиций, занимаемых участниками в отношении поставленных задач", 
+			"новых предложений", 
+			"направлений прогрессивного развития",
+			"перспективных возможностей и гаранантированных выгод",
+			"прямых прибылей и рисков"
+			));
+		
+		Random random = new Random();
+
+		int i1, i2, i3, i4, i1p, i2p, i3p, i4p;
+		Notepad newNotepad = Notepad.createNotepad();
+		
+		for (int i=0;i<n;i++){
+			
+			i1 = i==0 ? 0 : random.nextInt(start.size());
+			i2 = random.nextInt(actor.size());
+			i3 = random.nextInt(action.size());
+			i4 = random.nextInt(result.size());
+				
+			newNotepad.addRecord(start.get(i1) + " " + actor.get(i2) + " " + action.get(i3) + " " + result.get(i4) + ". ");
+				
+//				start.remove(i1);
+//				actor.remove(i2);
+//				action.remove(i3);
+//				result.remove(i4);
+				
+			i1p = i1;
+			i2p = i2;
+			i3p = i3;
+			i4p = i4;
+		}
+		return newNotepad;
+	}
+	
 	
 }
--- into_OOP/src/into_OOP/WrapNotepad.java
+++ into_OOP/src/into_OOP/WrapNotepad.java
@@ -4,6 +4,9 @@ public class WrapNotepad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Notepad Book = Notepad.generateNotepad(26);
+		System.out.println(Book);
+		Book.printNotepad();
 
 	}
 
