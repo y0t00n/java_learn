79003f2a2ae78f04ac8696d33632e0f07d25ef15
Ievgen Lauta <ievgen.lauta@gmail.com>
2016-03-22 17:03:26 +0200

    Multiply initial

---
+++ into_OOP/src/into_OOP/WrapMultiply.java
@@ -0,0 +1,10 @@
+package into_OOP;
+
+public class WrapMultiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- into_OOP/src/into_OOP/WrapNotepad.java
+++ into_OOP/src/into_OOP/WrapNotepad.java
@@ -4,7 +4,7 @@ public class WrapNotepad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Notepad Book = Notepad.generateNotepad(26);
+		Notepad Book = Notepad.generateNotepad(3);
 		System.out.println(Book);
 		Book.printNotepad();
 
